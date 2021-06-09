191f7d8c1d047b7a000fba415e5bdbefbeb5d6d9
Francisco Sousa <franciscoeds1987@gmail.com>
2021-06-09 10:20:52 -0300

    Entendendo o funcionamento de preflight do CORS

--- client.js
+++ client.js
@@ -3,9 +3,24 @@ function consultarRestaurantes() {
     $.ajax({
         url: "http://api.algafood.local:8080/restaurantes",
         type: "get",
+        headers: {
+            "X-Teste": "Abc"
+        },
 
         success: function (response) {
             $("#conteudo").text(JSON.stringify(response));
+        }
+    });
+}
+
+function fecharRestaurante() {
+    // noinspection JSUnusedGlobalSymbols
+    $.ajax({
+        url: "http://api.algafood.local:8080/restaurantes/1/fechamento",
+        type: "put",
+
+        success: function (response) {
+            alert("Restaurante foi fechado!");
         }
     });
 }
